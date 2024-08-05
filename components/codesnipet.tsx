719478f59253ebad9d12191e0960f91cc282e00c
laguagu <49702497+laguagu@users.noreply.github.com>
2024-08-05 18:06:46 +0300

    More themes to code snippet

--- components/codesnipet.tsx
+++ components/codesnipet.tsx
@@ -1,18 +1,25 @@
-"use client"
-import React, { useEffect, useRef } from 'react';
-import { FaCopy } from 'react-icons/fa';
-import hljs from 'highlight.js/lib/core';
-import javascript from 'highlight.js/lib/languages/javascript';
-import 'highlight.js/styles/monokai.css'; // Voit vaihtaa teeman tarpeen mukaan
+"use client";
+import React, { useEffect, useRef } from "react";
+import { FaCopy } from "react-icons/fa";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+// import 'highlight.js/styles/monokai.css'; // Voit vaihtaa teeman tarpeen mukaan
+// import "highlight.js/styles/github.css"; // Vaalea teema
+// import 'highlight.js/styles/tokyo-night-dark.css';
+import 'highlight.js/styles/tokyo-night-dark.css';
 
-hljs.registerLanguage('javascript', javascript);
+
+hljs.registerLanguage("javascript", javascript);
 
 interface CodeSnippetProps {
   code: string;
   language?: string;
 }
 
-const CodeSnippet: React.FC<CodeSnippetProps> = ({ code, language = 'javascript' }) => {
+const CodeSnippet: React.FC<CodeSnippetProps> = ({
+  code,
+  language = "javascript",
+}) => {
   const [isCopied, setIsCopied] = React.useState(false);
   const codeRef = useRef<HTMLElement>(null);
 
@@ -30,17 +37,17 @@ const CodeSnippet: React.FC<CodeSnippetProps> = ({ code, language = 'javascript'
   };
 
   return (
-    <div className="relative rounded-lg overflow-hidden inline-block">
+    <div className="relative rounded-lg overflow-hidden inline-block ">
       <pre className="p-4 bg-gray-800">
         <code ref={codeRef} className={`language-${language} text-sm`}>
           {code}
         </code>
       </pre>
-      <button 
-        onClick={copyToClipboard} 
+      <button
+        onClick={copyToClipboard}
         className="absolute top-2 right-2 bg-gray-700 hover:bg-gray-600 text-white rounded p-2 focus:outline-none"
       >
-        {isCopied ? 'Kopioitu!' : <FaCopy className="w-5 h-5" />}
+        {isCopied ? "Kopioitu!" : <FaCopy className="w-5 h-5" />}
       </button>
     </div>
   );
